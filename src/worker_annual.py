c544b07b843910f8df4c514377d098bf2fc47bc5
fgassert <cowbox314@gmail.com>
2016-07-22 20:34:03 -0400

    annual processing

---
+++ src/worker_annual.py
@@ -0,0 +1,60 @@
+import boto3
+import os
+import numpy as np
+import rasterio as rio
+import urllib
+
+def bandMean(arr):
+    return arr.mean(axis=0)[np.newaxis]
+def bandMin(arr):
+    return arr.min(axis=0)[np.newaxis]
+def bandMax(arr):
+    return arr.max(axis=0)[np.newaxis]
+def processTiff(inFile, outFile, f):
+    with rio.open(inFile, 'r', 'GTiff') as src:
+        profile = src.profile
+        arr = f(src.read())
+        profile['count'] = arr.shape[0]
+        profile['height'] = arr.shape[1]
+        profile['width'] = arr.shape[1]
+        with rio.open(outFile, 'w', **profile) as dst:
+            dst.write(arr)
+
+def worker(options):
+    try:
+        inUrl = options['inUrl']
+        outBucket = options['outBucket']
+        outKey = options['outKey']
+        var = options['var']
+        i = options['id']
+        tmp1 = 'tmp_{}'.format(i)
+        tmp2 = 'tmp2_{}'.format(i)
+
+        print ("{} Fetching {}".format(i,inUrl))
+        urllib.urlretrieve(inUrl, tmp1)
+
+        print ("{} Processing {}".format(i,inUrl))
+        f = {
+            "pr":bandMean,
+            "tasmax":bandMax,
+            "tasmin":bandMin
+        }[var]
+
+        processTiff(tmp1, tmp2, f)
+
+        print ("{} Putting {}".format(i,outKey))
+
+        s3 = boto3.client('s3')
+        s3.upload_file(tmp2, outBucket, outKey, {"ACL": "public-read"})
+
+        print ("{} Cleaning {}".format(i,outKey))
+        os.remove(tmp1)
+        os.remove(tmp2)
+
+    except Exception as e:
+        print (e, options)
+        try:
+            os.remove(tmp1)
+            os.remove(tmp2)
+        except:
+            pass
