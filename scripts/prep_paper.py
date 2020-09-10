15d93d639e95af466950d07d24b526b902ac608f
fgassert <fgassert@gmail.com>
2020-09-09 21:25:01 -0500

    add baselines

--- scripts/prep_paper.py
+++ scripts/prep_paper.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import sys
 import boto3
 import os
@@ -39,6 +40,11 @@ startyear = 2000
 endyear = 2080
 
 
+BUCKET = os.getenv('GDDP_BUCKET')
+PREFIX = os.getenv('GDDP_PREFIX') or 'tmp/nex-gddp/'
+ARCHIVE_PREFIX = 'prepdata/nex-gddp/'
+
+
 def raster_template(e, ch, i, s, y1, y2, d):
     if not d:
         return f'{e}-{ch}-{i}_{s}_ens_{y1}-{y2}.tif'
@@ -47,135 +53,71 @@ def raster_template(e, ch, i, s, y1, y2, d):
 def url_template(rast):
     return f'https://s3.amazonaws.com/md.cc/tmp/nex-gddp/{rast}'
 
-def main():
+
+def _iter_func(func):
     for d in datasets:
-        for i in temp_indicators:
-            for s in scenarios:
-                for y in range(startyear, endyear+1, 10):
-                    for ch in ['abs', 'diff']:
-                        for e in ensembles:
-                            y1 = y-15
-                            y2 = y+15
-                            print(raster_template(e, ch, i, s, y1, y2, d))
-        for i in pr_indicators:
-            for s in scenarios:
-                for y in range(startyear, endyear+1, 10):
-                    for ch in ['abs', 'ch']:
-                        for e in ensembles:
-                            y1 = y-15
-                            y2 = y+15
-                            print(raster_template(e, ch, i, s, y1, y2, d))
+        for s in scenarios:
+            for y in range(startyear, endyear+1, 10):
+                y1 = y-15
+                y2 = y+15
+                for e in ensembles:
+                    for i in temp_indicators:
+                        for ch in ['abs', 'diff']:
+                            func(e, ch, i, s, y1, y2, d)
+                    for i in pr_indicators:
+                        for ch in ['abs', 'ch']:
+                            func(e, ch, i, s, y1, y2, d)
+        for i in temp_indicators+pr_indicators:
+            func('q50', 'abs', i, 'historical', 1960, 1990, d)
+
+
+def main():
+    def _print_rasters(*args):
+        print(raster_template(*args))    
+    _iter_func(_print_rasters)
+
+
+def baselines():
+    for d in datasets:
+        for i in temp_indicators+pr_indicators:
+            print(raster_template('q50', 'abs', i, 'historical', 1960, 1990, d))
 
 def csv():
     print('indicator, scenario, year, change, ensemble, dataset, tiff, url')
-
-    for d in datasets:
-        for i in temp_indicators:
-            for s in scenarios:
-                for y in range(startyear, endyear+1, 10):
-                    for ch in ['abs', 'diff']:
-                        for e in ensembles:
-                            y1 = y-15
-                            y2 = y+15
-                            rast = raster_template(e, ch, i, s, y1, y2, d)
-                            url = url_template(rast)
-                            print(', '.join([i, s, str(y), ch, e, d, rast, url]))
-
-        for i in pr_indicators:
-            for s in scenarios:
-                for y in range(startyear, endyear+1, 10):
-                    for ch in ['abs', 'ch']:
-                        for e in ensembles:
-                            y1 = y-15
-                            y2 = y+15
-                            rast = raster_template(e, ch, i, s, y1, y2, d)
-                            url = url_template(rast)
-                            print(', '.join([i, s, str(y), ch, e, d, rast, url]))
+    def _print_csv_line(e, ch, i, s, y1, y2, d):
+        rast = raster_template(e, ch, i, s, y1, y2, d)
+        url = url_template(rast)
+        print(', '.join([i, s, str(y), ch, e, d, rast, url]))
+    _iter_func(_print_csv_line)
 
 
-def check_rename():
-    BUCKET = os.getenv('GDDP_BUCKET')
-    PREFIX = os.getenv('GDDP_PREFIX')
+def move_to_archive():
     session = boto3.session.Session(
         aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
         aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
     )
     client = session.resource('s3')
     bucket = client.Bucket(BUCKET)
-    
-    d = 'nexgddp'
-    for s in scenarios:
-        for y in range(startyear, endyear+1, 10):
-            y1 = y-15
-            y2 = y+15
-            for e in ensembles:
-                for i in temp_indicators:
-                    for ch in ['abs', 'diff']:
-                        rast0 = os.path.join(PREFIX, raster_template(e, ch, i, s, y1, y2, None))
-                        rast1 = os.path.join(PREFIX, raster_template(e, ch, i, s, y1, y2, d))
-                        try:
-                            bucket.Object(rast1).copy_from(CopySource=os.path.join(BUCKET, rast0))
-                            print(f'copy from {rast0} to {rast1}')
-                            bucket.Object(rast0).delete()
-                            print(f'delete {rast0}')
-                        except Exception as ex:
-                            print(ex)
-                            pass
 
-                for i in pr_indicators:
-                    for ch in ['abs', 'ch']:
-                        rast0 = os.path.join(PREFIX, raster_template(e, ch, i, s, y1, y2, None))
-                        rast1 = os.path.join(PREFIX, raster_template(e, ch, i, s, y1, y2, d))
-                        try:
-                            bucket.Object(rast1).copy_from(CopySource=os.path.join(BUCKET, rast0))
-                            print(f'copy from {rast0} to {rast1}')
-                            bucket.Object(rast0).delete()
-                            print(f'delete {rast0}')
-                        except Exception as ex:
-                            print(ex)
-                            pass
-
-def rename_hdd_cdd():
-    BUCKET = os.getenv('GDDP_BUCKET')
-    PREFIX = os.getenv('GDDP_PREFIX')
-    session = boto3.session.Session(
-        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
-    )
-    client = session.resource('s3')
-    bucket = client.Bucket(BUCKET)
+    def _archive(*args, dry_run=False):
+        rast = raster_template(e, ch, i, s, y1, y2, d)
+        rast0 = os.path.join(PREFIX, rast)
+        rast1 = os.path.join(ARCHIVE_PREFIX, rast)
+        try:
+            bucket.Object(rast1).copy_from(CopySource=os.path.join(BUCKET, rast0))
+            print(f'copy from {rast0} to {rast1}')
+        except Exception as ex:
+            print(ex, 'rast0')
+            pass
     
-    d = 'nexgddp'
-    for s in scenarios:
-        for y in range(startyear, endyear+1, 10):
-            y1 = y-15
-            y2 = y+15
-            for e in ensembles:
-                for d in datasets:
-                    for ch in ['abs', 'diff']:
-                        i1 = 'hdd65f-tasmin_tasmax'
-                        i2 = 'cdd65f-tasmin_tasmax'
-                        itmp = 'tmp'
-                        rast1 = os.path.join(PREFIX, raster_template(e, ch, i1, s, y1, y2, d))
-                        rasttmp = os.path.join(PREFIX, raster_template(e, ch, itmp, s, y1, y2, d))
-                        rast2 = os.path.join(PREFIX, raster_template(e, ch, i2, s, y1, y2, d))
-                        if rast1 not in [
-                            'tmp/nex-gddp/q50-abs-hdd65f-tasmin_tasmax_rcp45_ens_1985-2015_nexgddp.tif',
-                            'tmp/nex-gddp/q50-diff-hdd65f-tasmin_tasmax_rcp45_ens_1985-2015_nexgddp.tif'
-                        ]:
-                            print(f'copy from {rast1} to {rasttmp}')
-                            bucket.Object(rasttmp).copy_from(CopySource=os.path.join(BUCKET, rast1))
-                            print(f'copy from {rast2} to {rast1}')
-                            bucket.Object(rast1).copy_from(CopySource=os.path.join(BUCKET, rast2))
-                            print(f'copy from {rasttmp} to {rast2}')
-                            bucket.Object(rast2).copy_from(CopySource=os.path.join(BUCKET, rasttmp))
-                        
+    _iter_func(_archive)
+
 if __name__ == '__main__':
     if len(sys.argv)>1 and sys.argv[1]=='csv':
         csv()
-    elif len(sys.argv)>1 and sys.argv[1]=='rename':
-        check_rename()
-    elif len(sys.argv)>1 and sys.argv[1]=='fix_cdd':
-        rename_hdd_cdd()
+    elif len(sys.argv)>1 and sys.argv[1]=='archive':
+        move_to_archive()
+    elif len(sys.argv)>1 and sys.argv[1]=='baselines':
+        baselines()
     else:
         main()
